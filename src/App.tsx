17e2732e7b7ee404859758539269c9ed7eae026f
wuwei <eric.w.wu@mail.foxconn.com>
2025-02-15 20:27:07 +0800

    feat: 2 一网打尽组件常用hook 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+async function queryData() {
+  const data = await new Promise<number>((resolve) => {
+    setTimeout(() => {
+      resolve(666);
+    }, 2000);
+  })
+  return data;
+}
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  queryData().then(data => {
+    setNum(data);
+  })
+  useEffect(() => {
+    console.log(num);
+    // if (num > 0) {
+    // } else { 
+    //   console.log(2222);
+    // }
+  }, [num]);
+
+  // prevNum 是 上一次的 num
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
   );
 }
 
 export default App;
+
