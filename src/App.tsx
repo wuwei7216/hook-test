b2dfbc1059c43e004f1c6bde5bae79029ad704e2
wuwei <eric.w.wu@mail.foxconn.com>
2025-02-18 18:21:20 +0800

    feat: 2 一网打尽组件常用hook 02 完结

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useEffect, useState } from "react";
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import React from 'react';
+import { useImperativeHandle } from 'react';
 
-async function queryData() {
-  const data = await new Promise<number>((resolve) => {
-    setTimeout(() => {
-      resolve(666);
-    }, 2000);
-  })
-  return data;
+interface RefProps {
+  aaa: () => void;
 }
 
+const Guang: React.ForwardRefRenderFunction<RefProps> = (props, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      aaa() {
+        inputRef.current?.focus();
+      }
+    }
+  }, [inputRef]);
+
+  return <div>
+    <input ref={inputRef}></input>
+  </div>
+}
+
+const WrapedGuang = React.forwardRef(Guang);
+
 function App() {
-  const [num, setNum] = useState(0);
+  const ref = useRef<RefProps>(null);
+ 
+  useEffect(()=> {
+    console.log('ref', ref.current)
+    ref.current?.aaa();
+  }, []);
 
-  queryData().then(data => {
-    setNum(data);
-  })
-  useEffect(() => {
-    console.log(num);
-    // if (num > 0) {
-    // } else { 
-    //   console.log(2222);
-    // }
-  }, [num]);
-
-  // prevNum 是 上一次的 num
   return (
-    <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
+    <div className="App">
+      <WrapedGuang ref={ref}/>
+    </div>
   );
 }
 
 export default App;
-
